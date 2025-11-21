a65fc0963214a6616fc191157e1b16f2d2cafe6f
jonas <jonas.boucher@lakeregionschools.org>
2025-11-21 10:10:35 -0500

    Describe what you changed

---
+++ Push to gethub.py
@@ -0,0 +1,13 @@
+questions_data = [
+    {
+        "question": "who is the main protagonist of half-life 2",
+        "options": ["a) Alex Vance", "b) Gordon Freeman"],
+        "answer": "b"
+    },
+    {
+        "question": "what species is the main protagonist vertigo 2?",
+        "options": ["a) human alien", "b) human", "c) Alien"],
+        "answer": "a"
+    },
+    # Add more questions here
+]
